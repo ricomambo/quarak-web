c42080dad7bb0095c92bab1a0b718c2ff8d9b71f
Damian Farina <damianfarina@gmail.com>
2014-05-07 20:32:04 -0300

    First step for CRUD expenses

--- app/scripts/controllers/expense.js
+++ app/scripts/controllers/expense.js
@@ -1,6 +1,22 @@
 'use strict';
 
-angular.module('quarakWebApp')
-  .controller('ExpenseCtrl', function ($scope, Expense) {
+angular.module('quarak')
+  .controller('ExpenseCtrl', ['$scope', 'Expense', function ($scope, Expense) {
     $scope.expenses = Expense.query();
-  });
+    $scope.activeExpense = new Expense({date: new Date()});
+    $scope.save = function save(expense) {
+      if (!_.include($scope.expenses, expense)) {
+        $scope.expenses.push(expense);
+        expense.$save();
+      }
+      else {
+        Expense.update(expense);
+      }
+      $scope.activeExpense = new Expense({date: new Date()});
+    }
+    $scope.remove = function remove(expense) {
+      var index = $scope.expenses.indexOf(expense);
+      $scope.expenses.splice(index, 1);
+      expense.$remove();
+    }
+  }]);
