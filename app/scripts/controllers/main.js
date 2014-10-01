6440870268d12b9b8334e977b69afc829eeb4c7d
Mauricio Miranda <maurimiranda@gmail.com>
2014-10-01 12:36:51 -0300

    Remove hardcodead login user

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,12 +4,6 @@ angular.module('quarak')
   .controller('MainCtrl', ['$scope', 'Session', '$window', function ($scope, Session, $window) {
     $scope.Session = Session;
 
-    // TODO remove
-    $scope.user = {
-      email: 'ringo@test.com',
-      password: 'Star1234'
-    };
-
     $scope.signIn = function signIn(user) {
       Session.login(user.email, user.password);
     };
